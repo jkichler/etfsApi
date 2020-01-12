9e7e0193fe02308b5cc93d6b1c5fcc0ccfaad013
Justin Kichler <jkichler@gmail.com>
2020-01-12 13:43:18 -0500

    feat:db(added user table)

---
+++ db/models/user.js
@@ -0,0 +1,56 @@
+const crypto = require('crypto');
+const _ = require('lodash');
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const User = db.define('user', {
+  userName: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+  },
+  salt: {
+    type: Sequelize.STRING,
+  }
+})
+
+module.exports = User
+
+User.prototype.correctPassword = function(candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
+}
+
+User.prototype.sanitize = function () {
+  return _.omit(this.toJSON(), ['password', 'salt']);
+};
+
+
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64')
+}
+
+User.encryptPassword = function(plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex')
+}
+
+const setSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt()
+    user.password = User.encryptPassword(user.password, user.salt)
+  }
+}
+
+User.beforeCreate(setSaltAndPassword)
+User.beforeUpdate(setSaltAndPassword)
+User.beforeBulkCreate(users => {
+  users.forEach(setSaltAndPassword)
+})
+
+module.exports = User;
